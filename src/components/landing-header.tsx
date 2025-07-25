973e8a26518f2a5505887c0f780a745e7e4bd8cd
Rafael Emanuel <rafael.emanueldv.pro@gmail.com>
2025-07-25 14:21:14 -0400

    Add header to homepage

---
+++ src/components/landing-header.tsx
@@ -0,0 +1,17 @@
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+
+const LandingHeader = () => {
+	return (
+		<header className="container mx-auto px-4 py-4">
+			<div className="flex items-center justify-between">
+				<div className="flex items-center gap-2">
+					<Search className="h-6 w-8 text-primary" />
+					<h1 className="text-2xl font-bold text-primary">UFind</h1>
+				</div>
+			</div>
+		</header>
+	);
+}
+
+export default LandingHeader;
