75382267eacf0440e9f9f95c1fc7aefecafe1399
Rafael Emanuel <rafael.emanueldv.pro@gmail.com>
2025-07-27 11:48:04 -0400

    Add new variants to shadcn's button & update global.css

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,12 +14,13 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50",
+          "border border-primary bg-background text-primary hover:bg-primary hover:cursor-pointer hover:text-primary-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80",
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        filter: "bg-muted text-muted-foreground hover:cursor-pointer hover:bg-accent hover:text-accent-foreground border border-border"
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
